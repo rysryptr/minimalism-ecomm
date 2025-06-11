562ba396e13dd928c29c487a0ed85f9b5de9fce9
rysryptr <riyosuryaa@gmail.com>
2025-06-11 22:50:37 +0700

    fetch data from API

--- src/components/parts/HomePage/BrowseRoom.jsx
+++ src/components/parts/HomePage/BrowseRoom.jsx
@@ -1,6 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import useAsync from '../../../helpers/hooks/useAsync'
+import fetchData from '../../../helpers/fetch/fetch'
+
+function Loading({ ratio = {} }) {
+  const dummy = [
+    {
+      id: 1, 
+      ratio: {
+          default: "1/9",
+          md: "1/4"
+      }
+    },
+    {
+      id: 2, 
+      ratio: {
+          default: "1/9",
+          md: "2/2"
+      }
+    },
+    {
+      id: 3, 
+      ratio: {
+          default: "1/9",
+          md: "2/3"
+      }
+    },
+    {
+      id: 4, 
+      ratio: {
+          default: "1/9",
+          md: "1/4"
+      }
+    }
+  ]
+
+  return dummy.map( (item, index) => {
+    return <div key={item.id}
+            className={`relative card ${ratio?.wrapper.default?.[item.ratio.default]} ${ratio?.wrapper.md?.[item.ratio.md]}`}
+            style={{ height: index === 0 ? 180 : "auto" }}
+            >
+              <div className='bg-gray-300 rounded-lg w-full h-full'>
+                <div
+                  className={`overlay ${ratio?.meta?.[item.ratio.md]}`}
+                >
+                  <div className="w-24 h-3 bg-gray-400 rounded-full mt-3"></div>
+                  <div className="w-36 h-3 bg-gray-400 rounded-full mt-2"></div>
+                </div>
+              </div>
+          </div>
+  })
+}
+
 
 function BrowseRoom() {
+  const { data, status, error, run, isLoading, isSuccess } = useAsync({ data: { username: ""} })
+
+  useEffect(() => {
+    run(
+      fetchData({url: "/api/categories/?page=1&limit=4"})
+    )
+  }, [run])
+
+  // console.log(data)
+  
+  const ratioClassName = {
+    wrapper: {
+      default: {
+        "1/9": "col-span-9 row-span-1"
+      },
+      md: {
+        "1/4": "md:col-span-4 md:row-span-1",
+        "2/2": "md:col-span-2 md:row-span-2",
+        "2/3": "md:col-span-3 md:row-span-2",
+      }
+    },
+    meta: {
+      "1/9": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+      "1/4": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+      "2/2": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+      "2/3": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+    }
+  }
+
   return (
     <section className="flex bg-gray-100 py-16 px-4" id="browse-the-room">
       <div className="container mx-auto">
@@ -10,84 +92,29 @@ function BrowseRoom() {
           </h3>
         </div>
         <div className="grid grid-rows-2 grid-cols-9 gap-4">
-          <div
-            className="relative col-span-9 row-span-1 md:col-span-3 md:row-span-2 card"
-          >
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/image-catalog-4.png"
-                alt=""
-                className="w-full h-full object-cover object-center card-image overlay overflow-hidden rounded-xl"
-              />
+          {
+            isLoading ? <Loading ratio={ratioClassName} /> :
+            data.data.map( (item, index) => {
+              return <div key={item.id}
+              className={`relative card ${ratioClassName?.wrapper.default?.[item.ratio.default]} ${ratioClassName?.wrapper.md?.[item.ratio.md]}`}
+              style={{ height: index === 0 ? 180 : "auto" }}
+              >
+              <div className="card-shadow rounded-xl">
+                <img
+                  src={`./images/${item.imageUrl}`}
+                  alt={item.title}
+                  className="w-full h-full object-cover object-center card-image overlay overflow-hidden rounded-xl"
+                />
+              </div>
+              <div
+                className={`overlay ${ratioClassName?.meta?.[item.ratio.md]}`}
+              >
+                <h5 className="text-lg font-semibold">{item.title}</h5>
+                <span className="">{item.products} item{item.products > 1 ? "s" : ""}</span>
+              </div>
             </div>
-            <div
-              className="overlay right-0 left-0 top-0 bottom-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12"
-            >
-              <h5 className="text-lg font-semibold">Living Room</h5>
-              <span className="">22.094 items</span>
-            </div>
-            <a href="details.html" className="stretched-link">
-              
-            </a>
-          </div>
-          <div
-            className="relative col-span-9 row-span-1 md:col-span-2 md:row-span-2 card"
-          >
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/image-catalog-3.png"
-                alt=""
-                className="w-full h-full object-cover object-center card-image overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div
-              className="overlay right-0 left-0 top-0 bottom-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12"
-            >
-              <h5 className="text-lg font-semibold">Decoration</h5>
-              <span className="">77.392 items</span>
-            </div>
-            <a href="details.html" className="stretched-link">
-              
-            </a>
-          </div>
-          <div className="relative col-span-9 row-span-1 md:col-span-4 card">
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/image-catalog-2.png"
-                alt=""
-                className="w-full h-full object-cover object-center card-image overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div
-              className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72"
-            >
-              <h5 className="text-lg font-semibold">Children Room</h5>
-              <span className="">837 items</span>
-            </div>
-            <a href="details.html" className="stretched-link">
-            </a>
-          </div>
-          <div
-            className="relative col-span-9 row-span-1 md:col-span-4 card"
-            style={{height: 180}}
-          >
-            <div className="card-shadow rounded-xl">
-              <img
-                src="./images/image-catalog-1.png"
-                alt=""
-                className="w-full h-full object-cover object-center card-image overlay overflow-hidden rounded-xl"
-              />
-            </div>
-            <div
-              className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72"
-            > 
-              <h5 className="text-lg font-semibold">Living Room</h5>
-              <span className="">18.309 items</span>
-            </div>
-            <a href="details.html" className="stretched-link">
-              
-            </a>
-          </div>
+            })
+          }
         </div>
       </div>
     </section>
