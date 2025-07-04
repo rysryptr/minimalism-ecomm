7a84cc79f9dda6cada4e9559ea0864ad04576ce9
rysryptr <riyosuryaa@gmail.com>
2025-07-04 19:56:01 +0700

    fixing error

--- src/components/parts/Clients.jsx
+++ src/components/parts/Clients.jsx
@@ -7,18 +7,18 @@ function Clients() {
         <div
           className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
         >
-          <img src="./images/logo-adobe.svg" alt="" className="mx-auto" />
+          <img src={`${import.meta.env.BASE_URL}images/logo-adobe.svg`} alt="" className="mx-auto" />
         </div>
         <div
           className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
         >
-          <img src="./images/logo-ikea.svg" alt="" className="mx-auto" />
+          <img src={`${import.meta.env.BASE_URL}images/logo-ikea.svg`} alt="" className="mx-auto" />
         </div>
         <div
           className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
         >
           <img
-            src="./images/logo-hermanmiller.svg"
+            src={`${import.meta.env.BASE_URL}images/logo-hermanmiller.svg`}
             alt=""
             className="mx-auto"
           />
@@ -26,7 +26,7 @@ function Clients() {
         <div
           className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
         >
-          <img src="./images/logo-miele.svg" alt="" className="mx-auto" />
+          <img src={`${import.meta.env.BASE_URL}images/logo-miele.svg`} alt="" className="mx-auto" />
         </div>
       </div>
     </section>
